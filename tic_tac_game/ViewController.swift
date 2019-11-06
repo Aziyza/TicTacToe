e170e4daa591e7a84b62b98eb8b606f1a71ed1fa
Aziyza <baverly7@gmail.com>
2019-11-07 00:26:46 +0500

    Initial Commit

---
+++ tic_tac_game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tic_tac_game
+//
+//  Created by Mac on 11/7/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
